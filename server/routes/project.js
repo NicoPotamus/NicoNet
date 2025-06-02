616480420dab0921ea6be2d56bd8ffd674422f0d
nicopotamus <njdemilio@gmail.com>
2025-06-02 17:43:59 -0400

    js ts error?

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const project_1 = require("../controller/project");
-const router = (0, express_1.Router)();
+var express_1 = require("express");
+var project_1 = require("../controller/project");
+var router = (0, express_1.Router)();
 // Route to get batched projects with pagination
 router.get('/projects', project_1.getProjectsBatched);
 exports.default = router;
