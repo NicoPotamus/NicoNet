7c0a9b3f4733bdc90f234ce7627274c391f0b258
nicopotamus <njdemilio@gmail.com>
2025-06-02 00:11:26 -0400

    troubleshooting

--- client/src/app/components/myFetch.ts
+++ client/src/app/components/myFetch.ts
@@ -6,7 +6,7 @@ import {
     LoginResponse 
 } from '../model/project';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 /**
  * Fetch projects with pagination
